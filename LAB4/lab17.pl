6b4117e89f7acc6812c316e4d6fa509af315e160
lingraj <53535371+lingraj438@users.noreply.github.com>
2020-02-11 16:30:10 +0530

    Create lab17.pl

---
+++ LAB4/lab17.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;use warnings;
+my $DNA='ACCCCCCCCCCCC';
+my$i;
+my$mutant;
+srand(time|$$);
+$mutant=mutate($DNA);
+print"\nMutate DNA\n\n";
+print"\nHere is the original DNA:\n\n";
+print"$DNA\n";
+print"\nHere is the mutant DNA:\n\n";
+print"$mutant\n";
+print"\nHere are 10 more successive mutations:\n\n";
+for($i=0;$i<10;++$i){
+$mutant=mutate($mutant);
+print"$mutant\n";
+}
+exit;
+sub mutate {
+my($dna)=@_;
+my(@nucleotides)=('A','c','G','T');
+my($positions)=randomposition($dna);
+my($newbase)=randomnucleotide(@nucleotides);
+substr($dna,$positions,1,$newbase);
+return$dna;
+}
+sub randomelement{
+my(@array)=@_;
+return$array[rand @array];
+}
+sub randomnucleotide{
+my(@nucleotides)=('A','C','G','T');
+return randomelement(@nucleotides);
+}
+sub randomposition{
+my($string)=@_;
+return int(rand(length($string)));
+}
